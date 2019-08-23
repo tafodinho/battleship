deef4d28b0fbf8f4a1f478a1c4b7a711e507754c
offorma <offorma@gmail.com>
2019-08-23 14:07:55 +0100

    created a function to render board in game board

--- src/ultility.js
+++ src/ultility.js
@@ -1,28 +1,4 @@
 
-const renderBoard = (board, parent) => {
-    for(let i = 0; i < board.numRows; i++) {
-        const row = document.createElement("tr")
-        row.setAttribute("id", `row-${i}`)
-        row.setAttribute("class", "row")
-        for(let j = 0; j < board.numColumns; j++) {
-            const box = document.createElement('td')
-            box.setAttribute("id", `${board.owner}${i}${j}`)
-            box.setAttribute("class", "box")
-            row.appendChild(box)
-        }
-        parent.appendChild(row)
-    }
-    
-    if(board.owner === "c") {
-
-    } else {
-        board.ships.forEach(ship => {
-            placeShip(ship)
-        });
-    }
-    
-    
-}
 
 const placeShip = (ship) => {
     for(let i = 0; i < ship.location.length; i++) {
