16051b9b976c8eb800450cbdc5a10234a1147fcc
offorma <offorma@gmail.com>
2019-08-24 14:39:43 +0100

    merged

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import GameBoard from './GameBoard'
 import GamePlay from './gamePlay'
-import Ship from './ship'
+import Ship from './Ship'
 import { 
         markHitLocation, 
         clearMessage, 
