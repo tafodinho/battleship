9b065f2125b6d1a387aa7b0e7eb113d1bb834585
offorma <offorma@gmail.com>
2019-08-31 13:31:35 +0100

    masseges cleared after display

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import Player from './Player';
 
 const computerShips = generateShips("c")
 const playerShips = generateShips("p")
-console.log(computerShips)
 const playerParent = document.getElementById("player1-board")
 const computerParent = document.getElementById("player2-board")
 const container = document.getElementById("container")
@@ -37,33 +36,4 @@ displayBoard(playerBoard, playerParent)
 displayBoard(computerBoard, computerParent)
 });
 
-
-// clearMessage()
-// const createAShip = ((lengthOfShip, owner, orientation) => {
-//     let location = [];
-//     let firstDigit = null;
-//     let secondDigit = null;
-//     let spot = ""
-//     let i = 0
-//     while(location.length < lengthOfShip) {
-//         if(orientation == "horizontal") {
-//             if(location.length == 0) {
-//                 firstDigit = Math.floor(Math.random() * 10);
-//                 secondDigit = Math.floor(Math.random() * 10);
-//             } else {
-//                 secondDigit += 1
-//             }
-//         } else {
-//             if(location.length == 0) {
-//                 firstDigit = Math.floor(Math.random() * 10);
-//                 secondDigit = Math.floor(Math.random() * 10);
-//             } else {
-//                 firstDigit += 1
-//             }
-//         }
-       
-//         spot = owner+firstDigit+secondDigit
-//         location.push(spot)
-//     }
-//     return new Ship(location);
-// })(4, "p", "vertical")
+ clearMessage()
